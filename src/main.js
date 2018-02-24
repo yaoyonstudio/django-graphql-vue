51ccf448d1f213b0e1cc8d599cd4d13b38ef4a60
yaoyonstudio <yaoyonstudio@gmail.com>
2018-02-24 18:24:02 +0800

    'init'

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import './registerServiceWorker';
+import { apolloProvider } from './vue-apollo';
 
 Vue.config.productionTip = false;
 
 new Vue({
+  provide: apolloProvider.provide(),
   router,
   render: h => h(App),
 }).$mount('#app');
